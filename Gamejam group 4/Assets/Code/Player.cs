32a3779cdf5243fab36601fc3f1c613da5480089
472529 <472529@chrome.escg.ac.uk>
2021-01-25 12:16:40 +0000

    added player code from part 1

---
+++ Gamejam group 4/Assets/Code/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector2 targetPos;
+    public float Yincrement;
+
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.x < maxHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+            transform.position = targetPos;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+            transform.position = targetPos;
+        }
+    }
+}
